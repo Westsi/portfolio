fd1e7b2330ab3b45ff8674c6cdbbeae554daa196
Westsi <76999267+Westsi@users.noreply.github.com>
2022-10-09 19:39:02 +0100

    initial commit

---
+++ src/commands.js
@@ -0,0 +1,51 @@
+const help = [
+    {
+        command :"about",
+        description: "About me"
+    },
+    {
+        command :"contact",
+        description: "Contact me"
+    },
+    {
+        command :"projects",
+        description: "My Projects"
+    },
+    {
+        command :"skills",
+        description: "The languages, frameworks and tools that I have experience with and my other skills"
+    }
+]
+
+const about = [
+    "Hey There!",
+    "I am Jamie, a 'fullstack' developer from the UK and Canada."
+]
+
+const contact = [
+    {text: "Email: ", link: <a href="mailto:jamie@rockyroost.com" target="_blank">jamie@rockyroost.com</a>},
+    {text: "Discord: ", link: <a href="https://discord.com/users/688085162265018547" target="_blank">1Westsi#0412</a>},
+    {text: "Github: ", link: <a href="https://github.com/westsi" target="_blank">Westsi</a>},
+    {text: "Youtube: ", link: <a href="https://youtube.com/westsi" target="_blank">Westsi</a>}
+]
+
+const skills = [
+    {text: "Python", icon: "", yrsExp: 4, confidence: 7},
+    {text: "Javascript", icon: "", yrsExp: 2, confidence: 4},
+    {text: "Java", icon: "", yrsExp: 1, confidence: 5},
+    {text: "Golang", icon: "", yrsExp: 1, confidence: 4},
+    {text: "React", icon: "", yrsExp: 2, confidence: 7},
+    {text: "Flask/Flask-RESTful", icon: "", yrsExp: 2, confidence: 6},
+    {text: "Gin for Golang", icon: "", yrsExp: 1, confidence: 3},
+    {text: "Postman", icon: "", yrsExp: 3, confidence: 8},
+    {text: "Git", icon: "", yrsExp: 3, confidence: 6},
+    {text: "Video Editing", icon: "", yrsExp: 3, confidence: 5},
+    {text: "C", icon: "", yrsExp: 1, confidence: 2},
+]
+
+
+const projects = [
+    {name: <a href="https://github.com/westsi/" target="_blank"></a>, writtenIn: [], toolsUsed: [], description: ""},
+    {name: <a href="https://github.com/westsi/" target="_blank"></a>, writtenIn: [], toolsUsed: [], description: ""},
+    {name: <a href="https://github.com/westsi/" target="_blank"></a>, writtenIn: [], toolsUsed: [], description: ""},
+]
---
+++ src/terminal.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Terminal = () => {
+    const prompt = "WS C:\\Users\\visitor>"
+    
+    return(
+        <h1>{prompt}</h1>
+    )
+}
+
+export default Terminal;

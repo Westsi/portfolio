b832f15cc1910e04118fe5fb3ebe315d0baeac8c
Westsi <76999267+Westsi@users.noreply.github.com>
2022-10-12 15:43:58 +0100

    added lines

--- src/App.js
+++ src/App.js
@@ -1,19 +1,38 @@
-import Terminal, { updateLines } from "./terminal";
+import Terminal from "./terminal";
 import Art2 from "./art2";
 import React, { useState } from "react";
 
-function App() {
 
-  const [lines, setLines] = useState([]);
-  const updateLines = (line) => {
-    setLines(lines + line);
-  }
-  return (
-    <div className="App">
-      <Terminal lines={lines} />
-      <button onClick={updateLines("1")}>Click me!</button>
-    </div>
-  );
+function useForceUpdate(){
+    const [value, setValue] = useState(0); // integer state
+    return () => setValue(value => value + 1);
+}
+
+function App() {
+	const [lines, setLines] = useState(["hello"]);
+
+	const forceUpdate = useForceUpdate();
+	
+	
+	const updateLines = (line) => {
+		var lines2 = lines;
+		lines2.push(line);
+		setLines(lines2);
+		console.log(lines2);
+		console.log(lines);
+		console.log("lines updated!");
+		forceUpdate();
+	}
+	return (
+		<div className="App">
+			<h1>Hello!</h1>
+			{lines.map(line => {
+			return <h2>{line}</h2>
+		})}
+			<Terminal />
+			<button onClick={() => {updateLines("1")}}>Click me!</button>
+		</div>
+	);
 }
 
 export default App;
--- src/terminal.js
+++ src/terminal.js
@@ -5,9 +5,6 @@ const Terminal = (props) => {
     const prompt = "WS C:\\Users\\visitor>";
     return(
         <div className="app">
-            {props.lines.map(line => 
-                <h2 className="line">{line}</h2>
-                )}
             <h1>{prompt}</h1>
         </div>
     )

5e53416f97aaf55bcfb8a2d0df0a8d5823ae9b40
Westsi <76999267+Westsi@users.noreply.github.com>
2022-10-12 13:51:21 +0100

    lines

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import Terminal from "./terminal";
+import Terminal, {updateLines} from "./terminal";
 import Art2 from "./art2";
 
 function App() {
   return (
     <div className="App">
       <Terminal />
-      <Art2 />
+      <button onClick = {updateLines("1")}>Click me!</button>
     </div>
   );
 }
--- src/terminal.js
+++ src/terminal.js
@@ -1,10 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 
+
+const [lines, setLines] = useState([]);
+
+
+const updateLines = (line) =>{
+    setLines(lines + line);
+}
 const Terminal = () => {
-    const prompt = "WS C:\\Users\\visitor>"
-    
+    const prompt = "WS C:\\Users\\visitor>";
     return(
         <div className="app">
+            {lines.map(line => 
+                <h2 className="line">{line}</h2>
+                )}
             <h1>{prompt}</h1>
         </div>
     )
